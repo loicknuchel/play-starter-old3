43e25f5fac3871bad9a999580277bac1615c1fb8
Loïc Knuchel <loicknuchel@gmail.com>
2015-02-03 08:02:07 +0100

    add CORS to REST api

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,11 @@ object Application extends Controller {
     Ok(views.html.Application.home())
   }
 
+  def corsPreflight(all: String) = Action {
+    Ok("").withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Allow" -> "*",
+      "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" -> "Origin, X-Requested-With, Content-Type, Accept, Referrer, User-Agent");
+  }
 }
